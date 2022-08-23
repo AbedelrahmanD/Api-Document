8f7f7c32b08e543daba0622aae1340925e91fa48
AbedelrahmanD <68178460+AbedelrahmanD@users.noreply.github.com>
2022-08-24 00:26:04 +0300

    fav icon

--- Template/header.php
+++ Template/header.php
@@ -6,8 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <!-- <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap" rel="stylesheet"> -->
-
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="Template/assets/images/fav.png">
     <link href="Template/assets/css/select2.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="Template/assets/css/style.css">
     <script src="Template/assets/js/jquery.min.js"></script>
